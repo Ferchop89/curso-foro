0f4a940e3bb76b4d2cf68b66386ac976e06cec6b
Ferchop89 <fechopacheco@gmail.com>
2018-04-06 17:12:33 -0500

    Modificacion de Test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,18 +4,28 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function test_basic_example()
     {
-        $response = $this->get('/');
+        $user = factory(\App\User::class)->create([
+            'name' => 'Fernando Pacheco',
+            'email' => 'fpacheco@dgae.com.mx',
+        ]);
 
-        $response->assertStatus(200);
+
+
+        $this->actingAs($user, 'api')
+            ->get('api/user')
+            ->AssertSee('Fernando Pacheco')
+            ->AssertSee('fpacheco@dgae.com.mx');
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,11 +4,11 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 
 class ExampleTest extends TestCase
 {
-    use DatabaseTransactions;
+
     /**
      * A basic test example.
      *
@@ -16,16 +16,8 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $user = factory(\App\User::class)->create([
-            'name' => 'Fernando Pacheco',
-            'email' => 'fpacheco@dgae.com.mx',
-        ]);
+        $response = $this->get('/');
 
-
-
-        $this->actingAs($user, 'api')
-            ->get('api/user')
-            ->AssertSee('Fernando Pacheco')
-            ->AssertSee('fpacheco@dgae.com.mx');
+        $response->assertStatus(200);
     }
 }
